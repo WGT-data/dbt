33f830a76ed7a7aa1a2b7925ab8568a5a67ff47f
WGT-data <data@wgt.com>
2025-12-09 12:50:18 -0800

    Use fully qualified stage and format names

--- macros/load_adjust_events.sql
+++ macros/load_adjust_events.sql
@@ -2,16 +2,16 @@
 
     {% set ios_copy %}
         COPY INTO ADJUST_S3.DATA.IOS_EVENTS
-        FROM @ADJUST_S3_STAGE
-        FILE_FORMAT = (FORMAT_NAME = 'ADJUST_CSV_FORMAT')
+        FROM @ADJUST_S3.PUBLIC.ADJUST_S3_STAGE
+        FILE_FORMAT = (FORMAT_NAME = 'ADJUST_S3.PUBLIC.ADJUST_CSV_FORMAT')
         PATTERN = '.*acqu46kv92ss.*\.csv\.gz'
         ON_ERROR = 'CONTINUE';
     {% endset %}
 
     {% set android_copy %}
         COPY INTO ADJUST_S3.DATA.ANDROID_EVENTS
-        FROM @ADJUST_S3_STAGE
-        FILE_FORMAT = (FORMAT_NAME = 'ADJUST_CSV_FORMAT')
+        FROM @ADJUST_S3.PUBLIC.ADJUST_S3_STAGE
+        FILE_FORMAT = (FORMAT_NAME = 'ADJUST_S3.PUBLIC.ADJUST_CSV_FORMAT')
         PATTERN = '.*q9nlmhlmwjec.*\.csv\.gz'
         ON_ERROR = 'CONTINUE';
     {% endset %}
