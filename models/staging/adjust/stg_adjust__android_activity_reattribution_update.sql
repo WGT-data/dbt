7a83e3217e3ec4c75c17f50a3a528a8f807e2c28
wizard7558 <rb.sorenson55@gmail.com>
2026-01-19 14:05:04 -0800

    new incremental

--- models/staging/adjust/stg_adjust__android_activity_reattribution_update.sql
+++ models/staging/adjust/stg_adjust__android_activity_reattribution_update.sql
@@ -4,8 +4,8 @@
         database='ADJUST_S3',
         schema='DATA',
         alias='ANDROID_ACTIVITY_REATTRIBUTION_UPDATE',
-        unique_key=['GPS_ADID', 'CREATED_AT', 'RANDOM'],
-        incremental_strategy='merge',
+        incremental_strategy='append',
+        incremental_predicates=["DATE(DBT_INTERNAL_DEST.LOAD_TIMESTAMP) >= CURRENT_DATE - 1"],
         on_schema_change='append_new_columns'
     )
 }}
@@ -14,8 +14,5 @@ SELECT *
 FROM ADJUST_S3.DATA.ANDROID_EVENTS
 WHERE ACTIVITY_KIND = 'reattribution_update'
 {% if is_incremental() %}
-    AND LOAD_TIMESTAMP > IFNULL(
-        (SELECT MAX(LOAD_TIMESTAMP) FROM {{ this }} WHERE LOAD_TIMESTAMP > CURRENT_TIMESTAMP - INTERVAL '1 day'),
-        CURRENT_TIMESTAMP - INTERVAL '1 hour'
-    )
+    AND LOAD_TIMESTAMP > (SELECT MAX(LOAD_TIMESTAMP) FROM {{ this }})
 {% endif %}
